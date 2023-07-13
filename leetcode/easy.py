6b8b4428de5ad2390f2de77821dafb4553ab061b
Denis Piskarev <timchamkin1@yadnex.ru>
2023-07-13 14:25:32 +0300

    new task

--- leetcode/easy.py
+++ leetcode/easy.py
@@ -3,6 +3,12 @@ from typing import List
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        '''
+        https://leetcode.com/problems/counting-bits/description/
+        Given an integer n, return an array ans of length n + 1
+        such that for each i (0 <= i <= n), ans[i] is the number
+        of 1's in the binary representation of i.
+        '''
         result = []
         for i in range(n + 1):
             result += [0]
---
+++ leetcode/medium.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Given an integer array nums, rotate the array to the
+        right by k steps, where k is non-negative.
+        Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) < k:
+            for _ in range(k):
+                nums[:] = [nums.pop()] + nums
+        else:
+            nums[:] = nums[-k:] + nums[:-k]
+        return nums
